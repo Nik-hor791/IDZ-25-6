d65e826a96dbcf1caf981e02ac8eee12dcaa31c2
Nikita <m2504025@edu.misis.ru>
2025-10-02 11:52:15 +0300

    домашка

--- src/lab02/02_matrix.py
+++ src/lab02/02_matrix.py
@@ -2,20 +2,62 @@ def transpose(mat):
     if mat == []:
         return mat
     else:
+        #Проверка на рванность
         l_mat = len(mat[0])
+        ok = 1
         for i in mat:
-            ok = 1
-            if i != l_mat:
+            if len(i) != l_mat:
                 ok = 0
+            else:
+                ok = 1
+        #Проверка на рванность
         if ok == 1:
+
             trans = [[mat[j][i] for j in range(len(mat))] for i in range(len(mat[0]))]
-    return trans
+            return trans
+        else:
+            return ('ValueError')
+
 
 def row_sums(mat):
-    return mat
+    # Проверка на рванность
+    l_mat = len(mat[0])
+    ok = 1
+    for i in mat:
+        if len(i) != l_mat:
+            ok = 0
+        else:
+            ok = 1
+    # Проверка на рванность
+    if ok == 1:
+        t = list()
+        for i in mat:
+            s = sum(i)
+            t = t + [s]
+        return t
+    else: ('ValueError')
+
 
 def col_sums(mat):
-    return mat
+    # Проверка на рванность
+    l_mat = len(mat[0])
+    ok = 1
+    for i in mat:
+        if len(i) != l_mat:
+            ok = 0
+        else:
+            ok = 1
+    # Проверка на рванность
+    if ok == 1:
+        t = list()
+        i_num_sum = 0
+        for num in range(0, l_mat):
+            for i in mat:
+                i_num_sum = i_num_sum + i[num]
+
+            t = t + [i_num_sum]
+            i_num_sum = 0
+        return t
 
 print(transpose([[1, 2, 3]]))
 print(transpose([[1], [2], [3]]))
